df19b64cf08b52d8019f79dfe6212e76182f9690
SemantaSetuu <127374854+SemantaSetuu@users.noreply.github.com>
2026-09-14 16:12:41 +0000

    commit

---
+++ src/main/java/com/learning/miniblog/service/PostService.java
@@ -0,0 +1,33 @@
+package com.learning.miniblog.service;
+
+import com.learning.miniblog.entity.Post;
+import com.learning.miniblog.repository.PostRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+
+    public PostService(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public List<Post> getAllPosts() {
+        return postRepository.findAll();
+    }
+
+    public Post getPostById(Long id) {
+        return postRepository.findById(id).orElse(null);
+    }
+
+    public Post createPost(Post post) {
+        return postRepository.save(post);
+    }
+
+    public void deletePost(Long id) {
+        postRepository.deleteById(id);
+    }
+}
