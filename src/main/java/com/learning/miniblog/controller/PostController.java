a69b02bf31c15509ee60fbb26c1fe677a7afd569
SemantaSetuu <127374854+SemantaSetuu@users.noreply.github.com>
2026-09-14 16:26:55 +0000

    new commit

---
+++ src/main/java/com/learning/miniblog/controller/PostController.java
@@ -0,0 +1,38 @@
+package com.learning.miniblog.controller;
+
+import com.learning.miniblog.entity.Post;
+import com.learning.miniblog.service.PostService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public List<Post> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("/{id}")
+    public Post getPostById(@PathVariable Long id) {
+        return postService.getPostById(id);
+    }
+
+    @PostMapping
+    public Post createPost(@RequestBody Post post) {
+        return postService.createPost(post);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePost(@PathVariable Long id) {
+        postService.deletePost(id);
+    }
+}
